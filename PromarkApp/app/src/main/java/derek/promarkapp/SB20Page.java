6b8b13fa129e2d711503fefa1eda21144ff989ac
Derek Fusari <derek.fusari@gmail.com>
2017-12-06 15:51:30 -0500

    Added SB20 Layout

---
+++ PromarkApp/app/src/main/java/derek/promarkapp/SB20Page.java
@@ -0,0 +1,13 @@
+package derek.promarkapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SB20Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sb20_page);
+    }
+}
