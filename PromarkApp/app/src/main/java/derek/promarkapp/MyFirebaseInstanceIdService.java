95ff7e50a774f4c28ed15ffb89a336115270b5e9
Derek Fusari <derek.fusari@gmail.com>
2018-01-03 16:19:05 -0500

    Firebase
    Added firebase to send messages to all app users

---
+++ PromarkApp/app/src/main/java/derek/promarkapp/MyFirebaseInstanceIdService.java
@@ -0,0 +1,24 @@
+package derek.promarkapp;
+
+/**
+ * Created by Derek-PC.
+ */
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+
+public class MyFirebaseInstanceIdService extends com.google.firebase.iid.FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token in logcat
+        Log.e(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+}
